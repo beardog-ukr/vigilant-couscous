91b518d50404a65dc36f52b7e8b3677e76297740
Your Name <you@example.com>
2019-09-27 22:16:50 +0300

    Added: tileset for metainfo

---
+++ SwordsmanAndSpiders/Resources/maps/meta_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="meta_tiles" tilewidth="32" tileheight="32" tilecount="11" columns="11">
+ <image source="meta_tiles_32x32_v01.png" width="352" height="32"/>
+</tileset>
