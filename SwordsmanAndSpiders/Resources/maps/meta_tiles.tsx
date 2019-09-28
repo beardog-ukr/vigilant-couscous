ec9183840fe044d429ddf2cf3fbbd38cc3168053
Your Name <you@example.com>
2019-09-28 22:50:26 +0300

    Added: basic map with metainfo

--- SwordsmanAndSpiders/Resources/maps/meta_tiles.tsx
+++ SwordsmanAndSpiders/Resources/maps/meta_tiles.tsx
@@ -1,4 +1,59 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="meta_tiles" tilewidth="32" tileheight="32" tilecount="11" columns="11">
  <image source="meta_tiles_32x32_v01.png" width="352" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="MetaTileCode" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="MetaTileCode" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="MetaTileCode" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="MetaTileCode" type="int" value="22"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="MetaTileCode" type="int" value="23"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="MetaTileCode" type="int" value="24"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="MetaTileCode" type="int" value="25"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="MetaTileCode" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="MetaTileCode" type="int" value="11"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="MetaTileCode" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="MetaTileCode" type="int" value="14"/>
+  </properties>
+ </tile>
 </tileset>
